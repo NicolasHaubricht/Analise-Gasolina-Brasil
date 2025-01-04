111d4b32a7a0cc863d7b240d26208864eec59ff9
Nicolas <haubrichtnicolas@gmail.com>
2025-01-04 17:53:01 -0300

    Imgs

--- ex08.py
+++ ex08.py
@@ -7,4 +7,5 @@ arquivo = pd.read_csv('gasolina_brasil.csv')
 df = pd.DataFrame(arquivo)
 df_2 = df[['ANO E MES', 'PREÇO MÉDIO REVENDA']]
 filtro = df_2[df_2['ANO E MES'] == '2008-08']
+print(filtro)
 print('Preço Médio de Revenda em Agosto de 2008: R$', filtro['PREÇO MÉDIO REVENDA'].mean())
--- ex11.py
+++ ex11.py
@@ -8,4 +8,5 @@ df = pd.DataFrame(arquivo)
 df_2 = df[['ANO E MES', 'PREÇO MÉDIO REVENDA', 'REGIÃO', 'PRODUTO']]
 filtro = df_2[(df_2['REGIÃO'] == 'SUL') & (df_2['ANO E MES'] >= '2012-01') & (df_2['ANO E MES'] <= '2012-12') & (df_2['PRODUTO'].isin(['GASOLINA COMUM', 'GASOLINA ADITIVADA']))]
 filtro = pd.DataFrame(filtro)
+print(filtro)
 print('Preço Médio de Revenda da gasolina nos Estados do Sul no ano de 2012: R$', filtro['PREÇO MÉDIO REVENDA'].mean())
